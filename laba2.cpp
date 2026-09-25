2ff7981c8f6995940358516305ef99671d78629e
zlynn-ml <zahar02042009136@gmail.com>
2026-09-25 10:34:43 +0300

    Initial commit: рабочая версия заданий

---
+++ laba2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	double x;
+	cin >> x;
+	if (x < 0)
+		cout << x * x;
+	else if (x == 0)
+		cout << "1";
+	else
+		cout << 1 / x;
+	return 0;
+}
---
+++ laboratornaya_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "");
+	double meter;
+	int answer;
+	cout << "Введите расстояние в метрах:\n";
+	cin >> meter;
+	answer = (int) (meter / 1000);
+	meter -= answer * 1000;
+	cout << "Расстояние в километрах: " << answer << " километров, " << meter << " метров";
+	return 0;
+}
